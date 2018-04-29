3b0c4efa84e17abfc519129420e09adb43dbb29a
Sam Boysel <sboysel@gmail.com>
2018-04-28 19:17:54 -0700

    refactor

--- R/spot.R
+++ R/spot.R
@@ -1,10 +1,10 @@
-#' Get spot information from the Spitcast API 
-#' 
-#' @param item The name of the specific spot data item as a string.  Must must 
+#' Get spot information from the Spitcast API
+#'
+#' @param item The name of the specific spot data item as a string.  Must must
 #' be one of 'all', 'forecast', 'nearby', 'neighbors', 'top', or 'search'.
-#' Required.
+#' Required.  Default is 'all'.
 #' @param spot_id The spot ID to query, as an integer.  Required for the
-#' 'forecast', and 'neighbors' items.
+#' 'forecast', and 'neighbors' items.  Default is \code{NULL}.
 #' @param ... Any additional parameters passed to specific data items.  For
 #' example, the 'neighbors' item takes an optional 'direction' parameter.
 #' @examples
@@ -14,26 +14,38 @@
 #' spot(item = "neighbors", spot_id = 1, direction = "above")
 #' spot(item = "forecast", spot_id = 1)
 #' @export
-spot <- function(item, spot_id = "", ...) {
+spot <- function(item = "all", spot_id = NULL, ...) {
+
   if (!(item %in% c("all", "forecast", "nearby", "neighbors",
                     "top", "search"))) {
-    stop(paste("item parameter must be one of 'all', 'forecast', 'nearby'",
-         "'neighbors', 'top', 'search'"))
+    stop(paste("`item`` parameter must be one of 'all', 'forecast', 'nearby'",
+         "'neighbors', 'top', or 'search'."))
   }
+
+  if (item %in% c("forecast", "neighbors") & is.null(spot_id)) {
+    stop("`spot_id` parameter is required for the 'forecast', and 'neighbors' items.")
+  }
+
   spitcast_spot <- "http://api.spitcast.com/api/spot"
-  req <- switch(item,
-                all = paste0(spitcast_spot, "/all"),
-                forecast = paste0(spitcast_spot, "/forecast/", spot_id),
-                nearby = paste0(spitcast_spot, "/nearby", nearby_req()),
-                neighbors = paste0(spitcast_spot, "/neighbors/",
-                  neighbors_req(spot_id, ...)),
-                search = search_req(...),
-                top = "http://api.spitcast.com/api/top/spots/"
+
+  req <- switch(
+    item,
+    all = paste0(spitcast_spot, "/all"),
+    forecast = paste0(spitcast_spot, "/forecast/", spot_id),
+    nearby = paste0(spitcast_spot, "/nearby", nearby_req()),
+    neighbors = paste0(spitcast_spot, "/neighbors/",
+    neighbors_req(spot_id, ...)),
+    search = search_req(...),
+    top = "http://api.spitcast.com/api/top/spots/"
   )
+
   resp <- jsonlite::fromJSON(url(req), flatten = TRUE)
+
   if ("coordinates" %in% names(resp)) {
     return(unnest_coords(resp))
   } else {
     return(resp)
   }
+
 }
+
--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,44 @@
-# Format current location for 'nearby' item request
+#' Format current location for 'nearby' item request
 nearby_req <- function() {
   geoip <- "http://freegeoip.net/json/"
   geoip_data <- jsonlite::fromJSON(url(geoip))
-  return(paste0("?longitude=", geoip_data$longitude, "&latitude=",
-    geoip_data$latitude))
+  geoip_query <- paste0(
+    "?longitude=", geoip_data$longitude,
+    "&latitude=", geoip_data$latitude
+  )
+  return(geoip_query)
 }
 
-# Format 'neighbors' item request
-neighbors_req <- function(spot_id, ...) {
-  if (identical(spot_id, "")) {
-    stop("You must provide a non-empty 'spot_id' parameter")
+#' Format 'neighbors' item request
+neighbors_req <- function(spot_id = NULL, ...) {
+
+  if (is.null(spot_id)) {
+    stop("`spot_id` parameter not found.")
   }
+
   params <- list(...)
+
   if ("direction" %in% names(params)) {
+
     if (!(params[["direction"]] %in% c("above", "below"))) {
-      stop(paste("The 'direction' parameter must be one of 'above'",
-                 "or 'below'"))
+
+      stop(paste("The `direction`` parameter must be one of 'above'",
+                 "or 'below'."))
+
     } else {
+
       return(paste0(spot_id, "/?direction=", params[["direction"]]))
+
     }
   } else {
+
     return(spot_id)
+
   }
+
 }
 
-# Format any additional 'search' item parameters
+#' Format any additional 'search' item parameters
 search_req <- function(...) {
   params <- list(...)
   params_req <- paste0("&", names(params), "=", params, collapse = "")
@@ -33,10 +47,10 @@ search_req <- function(...) {
           params_req))
 }
 
-# Unnest any long / lat pairs to columns
+#' Unnest any long / lat pairs to columns
 unnest_coords <- function(df) {
   if (!("coordinates" %in% names(df))) {
-    stop("'coordinates' not found")
+    stop("`coordinates` parameter not found.")
   }
   coords <- do.call(rbind, df[["coordinates"]])
   df$longitude <- coords[, 1]
